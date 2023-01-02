5c845fb8d6f9fb3720d0bdcfdfbc6e4035809acd
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-02 09:58:59 -0500

    feat(Seed): extract pokemon from pokeapi

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces';
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
 
-  executeSeed() {
-    return `seed executed`;
+  async executeSeed() {
+    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+  
+    const pokemons = data.results.map(({name, url}) => ({
+        name,
+        no: +url.split('/')[url.split('/').length - 2],
+    }))
+
+    return pokemons; 
   }
-
 }
