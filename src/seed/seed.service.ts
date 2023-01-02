7463b5a8560fe7b4a90b0dac02cf01938c32c788
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-02 10:15:06 -0500

    feat(Seed): save pokemons in the db

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+
 import { PokeResponse } from './interfaces';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
   
     const pokemons = data.results.map(({name, url}) => ({
         name,
         no: +url.split('/')[url.split('/').length - 2],
     }))
 
+    await this.pokemonModel.deleteMany();
+    await this.pokemonModel.insertMany(pokemons);
     return pokemons; 
   }
 }
