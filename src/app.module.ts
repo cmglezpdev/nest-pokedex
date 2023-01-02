b9474947f4af323c4ff038f4fad0aede3ac047d6
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-02 09:46:51 -0500

    feat(Root): add seed module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { CommonModule } from './common/common.module';
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
-    CommonModule
+    CommonModule,
+    SeedModule
   ],
   controllers: [],
   providers: [],
