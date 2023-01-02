cbc24cdde9a8df1552c4245d0a0a336f449a2a36
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-01 21:37:05 -0500

    feat(common): add MongoId Pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname,'..', 'public'),
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
-    PokemonModule
+    PokemonModule,
+    CommonModule
   ],
   controllers: [],
   providers: [],
