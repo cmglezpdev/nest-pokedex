e96a268dc604166fc469c8f74e31a927ad969988
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-01 21:20:00 -0500

    feat(Pokemon): delete a pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -64,8 +64,9 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
   }
 
   private handleException(error: any) {
