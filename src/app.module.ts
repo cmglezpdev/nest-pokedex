edac7184d01b74131b0a6e7646a103d290cfa9f0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-02 11:29:25 -0500

    chore(Root): use env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { envConfiguration } from './config/app.config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [envConfiguration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     PokemonModule,
     CommonModule,
     SeedModule
