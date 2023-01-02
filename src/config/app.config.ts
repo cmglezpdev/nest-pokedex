2b1368db3d88c82ad56f8cf901648a74a142e5ec
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-02 18:39:49 -0500

    fix(Root): convert a number

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,6 +4,6 @@ export const envConfiguration = () => ({
     enviroment: process.env.NODE_ENV || 'development',
     mongodb: process.env.MONGODB_URL,
     port: process.env.PORT || 3000,
-    defaultLimit: process.env.DEFAULT_LIMIT || 10,
-    defaultOffset: process.env.DEFAULT_OFFSET || 0,
+    defaultLimit: +process.env.DEFAULT_LIMIT || 10,
+    defaultOffset: +process.env.DEFAULT_OFFSET || 0,
 })
