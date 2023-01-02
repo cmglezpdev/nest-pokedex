739f44a4d6440b17ed2882c8bb001e23b119e84e
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2023-01-01 20:57:32 -0500

    feat(Pokemon): Get pokemon by...

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,6 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -33,8 +33,26 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if( !isNaN(+term) ) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+    
+    if( !pokemon && isValidObjectId(term) ) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+    
+    if( !pokemon ) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() });
+    }
+    
+    if( !pokemon ) {
+      throw new NotFoundException(`Pokemon with id, name or no "${term}" not found`)
+    }
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
